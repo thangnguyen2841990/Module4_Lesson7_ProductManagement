2f7ea1c198c25eaa7500ba918fb00cfd4b543649
thangnguyen2841990 <96616281+thangnguyen2841990@users.noreply.github.com>
2022-03-24 20:24:17 +0700

    done

---
+++ src/main/java/com/codegym/repository/category/ICategoryRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.repository.category;
+
+import com.codegym.model.Category;
+import com.codegym.repository.IGeneralRepository;
+
+public interface ICategoryRepository extends IGeneralRepository<Category> {
+}
---
+++ src/main/java/com/codegym/repository/product/IProductRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.repository.product;
+
+import com.codegym.model.Product;
+import com.codegym.repository.IGeneralRepository;
+
+public interface IProductRepository extends IGeneralRepository<Product> {
+}
